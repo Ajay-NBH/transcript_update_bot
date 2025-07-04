784de245fb6bd2c27f4d9fe85c3ee738e473286c
Mrityunjay Pandey <mrityunjaypandey021@gmail.com>
2025-07-04 12:25:09 +0530

    Updated requirements.txt

--- transcript_update_bot.py
+++ transcript_update_bot.py
@@ -29,8 +29,8 @@ SCOPES = [
     'https://www.googleapis.com/auth/documents'
 ]
 
-token = os.getenv(GOOGLE_TOKEN, "brand_vmeet_token.json")
-CREDENTIALS_FILE = os.getenv(GOOGLE_CREDENTIALS, "brand_vmeet_credentials.json")
+token = os.getenv("GOOGLE_TOKEN", "brand_vmeet_token.json")
+CREDENTIALS_FILE = os.getenv("GOOGLE_CREDENTIALS", "brand_vmeet_credentials.json")
 creds = None
 if os.path.exists(token):
     creds = Credentials.from_authorized_user_file(token, SCOPES)
