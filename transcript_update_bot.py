6ec5a5bd0307237fe85374faa3304f0dea03f0eb
Olivia Saha <olivia.saha@nobroker.in>
2025-12-31 14:51:58 +0530

    Update transcript_update_bot.py

--- transcript_update_bot.py
+++ transcript_update_bot.py
@@ -29,9 +29,8 @@ import pandas as pd
 SCOPES = [
     'https://www.googleapis.com/auth/calendar',
     'https://www.googleapis.com/auth/gmail.send',
-    'https://www.googleapis.com/auth/drive.readonly',
+    'https://www.googleapis.com/auth/drive',  # This includes drive.readonly
     'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/documents'
 ]
 
